5edabdc06f9a688c732e9fd20b282231d9ee26ad
falconlee236 <falconlee236@cau.ac.kr>
2024-04-07 13:15:09 +0900

    remove unnecessary code

--- src/logged_out/components/home/HeadSection.js
+++ src/logged_out/components/home/HeadSection.js
@@ -1,7 +1,7 @@
 import React, {Fragment, useState} from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import {Grid, Typography, Card, Button, Hidden, Box, Modal} from "@mui/material";
+import {Grid, Typography, Card, Button, Hidden, Box} from "@mui/material";
 import withStyles from "@mui/styles/withStyles";
 import WaveBorder from "../../../shared/components/WaveBorder";
 import ZoomImage from "../../../shared/components/ZoomImage";
