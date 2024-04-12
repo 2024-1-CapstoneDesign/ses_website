c713a6b6b681ed62ae7c262dded5bd2e8ac7baef
falconlee236 <falconlee236@cau.ac.kr>
2024-04-12 17:09:41 +0900

    add data format comment

--- src/logged_out/components/home/FeatureSection.js
+++ src/logged_out/components/home/FeatureSection.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React from "react";
 import {Grid, Typography} from "@mui/material";
 import CodeIcon from "@mui/icons-material/Code";
 import BuildIcon from "@mui/icons-material/Build";
@@ -16,7 +16,7 @@ import FeatureCard from "./FeatureCard";
 import useWidth from "../../../shared/functions/useWidth";
 import axios from "axios";
 import {useQuery} from "react-query";
-import WaveSurfer from "wavesurfer.js";
+import WaveSurferComponent from "./WaveSurferComponent";
 
 const iconSize = 30;
 
@@ -95,12 +95,48 @@ const features = [
   },
 ];
 
+/**
+ * 필요한것? soundEffectName, url, tagList, length
+ * url = https://soundeffect-search.p-e.kr/api/v1/soundeffect
+ * result
+ * {
+ *             "soundEffectId": 1,
+ *             "soundEffectName": "test",
+ *             "description": null,
+ *             "createBy": null,
+ *             "createdAt": null,
+ *             "soundEffectTags": [
+ *                 {
+ *                     "tagId": 1,
+ *                     "tagName": "sunny"
+ *                 },
+ *                 {
+ *                     "tagId": 2,
+ *                     "tagName": "happy"
+ *                 },
+ *                 {
+ *                     "tagId": 3,
+ *                     "tagName": "funny"
+ *                 }
+ *             ],
+ *             "soundEffectTypes": [
+ *                 {
+ *                     "url": "https://soundeffectsearch.s3.ap-northeast-2.amazonaws.com/ILLIT+(%E1%84%8B%E1%85%A1%E1%84%8B%E1%85%B5%E1%86%AF%E1%84%85%E1%85%B5%E1%86%BA)+%E2%80%98Magnetic%E2%80%99+Visual+Teaser.wav",
+ *                     "soundEffectTypeName": "wav",
+ *                     "length": null
+ *                 }
+ *             ]
+ *         }
+ * */
+
 const fetchSoundList = async () => {
-  const url = "https://soundeffect-search.p-e.kr/api/v1/test/download"
+  const url = "https://soundeffect-search.p-e.kr/api/v1/soundeffect"
   try {
     const res = await axios.get(url);
-    // console.dir(res)
-    return res.data;
+    if (res.data.result === "SUCCESS"){
+      return res.data;
+    }
+    return null;
   } catch (e){
     console.error(e);
     throw e;
@@ -119,34 +155,7 @@ function FeatureSection(props) {
   } = useQuery('soundList', fetchSoundList, {
     refetchOnWindowFocus: false
   });
-  const waveform = useRef(null);
-  const wavesurfer = useRef(null);
   const audioURL = "https://soundeffectsearch.s3.ap-northeast-2.amazonaws.com/ILLIT+(%E1%84%8B%E1%85%A1%E1%84%8B%E1%85%B5%E1%86%AF%E1%84%85%E1%85%B5%E1%86%BA)+%E2%80%98Magnetic%E2%80%99+Visual+Teaser.wav"
-  // const audioURL = "src/logged_out/dummy_data/example.wav";
-  const [colorType, setColorType] = useState(null);
-
-  useEffect(() => {
-    if(waveform.current){
-      // waveform.current가 읽혀지면 시작됨
-      wavesurfer.current =
-        WaveSurfer.create({
-          container: waveform.current, // ref로 연결했기 때문에 current로 연결해주기
-          barWidth: 4, // 막대 하나의 width값
-          barHeight: 1,
-          barGap: 2,
-          progressColor: colorType,
-          waveColor: '#FF00FF',
-          width: '50%'
-        });
-      console.dir(wavesurfer.current)
-      wavesurfer.current.load(audioURL);
-      wavesurfer.current.on('click', () => {
-        // https://wavesurfer-js.org/docs/methods.html
-        wavesurfer.current.setVolume(0.5);
-        wavesurfer.current.play()
-      })
-    }
-  },[])
 
   return (
     <div style={{ backgroundColor: "#FFFFFF" }}>
@@ -154,7 +163,7 @@ function FeatureSection(props) {
         <Typography variant="h3" align="center" className="lg-mg-bottom">
           Features
         </Typography>
-        <div ref={waveform}>hello</div>
+        <WaveSurferComponent audioURL={audioURL} />
         {isLoading && <strong>Loading....</strong>}
         {isError && <strong>{response?.errorMessage}</strong>}
         <div className="container-fluid">
