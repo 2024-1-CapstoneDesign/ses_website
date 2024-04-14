dfe44ff3bfd85130e2b60ec3ceb21c9007f03f2c
falconlee236 <falconlee236@cau.ac.kr>
2024-04-14 17:27:42 +0900

    add routing to blog page

--- src/logged_out/components/home/MoreButton.js
+++ src/logged_out/components/home/MoreButton.js
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import ButtonBase from '@mui/material/ButtonBase';
 import Typography from '@mui/material/Typography';
+import {Link} from "react-router-dom";
 
 const imageURL = `${process.env.PUBLIC_URL}/images/logged_out/headPhones.jpg`
 
@@ -78,33 +79,35 @@ const ImageMarked = styled('span')(({ theme }) => ({
 
 export default function MoreButton() {
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
-      <ImageButton
-        focusRipple
-        key={image.title}
-        style={{
-          width: image.width,
-        }}
-      >
-        <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
-        <ImageBackdrop className="MuiImageBackdrop-root" />
-        <Image>
-          <Typography
-            component="span"
-            variant="subtitle1"
-            color="inherit"
-            sx={{
-              position: 'relative',
-              p: 4,
-              pt: 2,
-              pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-            }}
-          >
-            {image.title}
-            <ImageMarked className="MuiImageMarked-root" />
-          </Typography>
-        </Image>
-      </ImageButton>
-    </Box>
+    <Link to="/blog">
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
+        <ImageButton
+          focusRipple
+          key={image.title}
+          style={{
+            width: image.width,
+          }}
+        >
+          <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+          <ImageBackdrop className="MuiImageBackdrop-root" />
+          <Image>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              sx={{
+                position: 'relative',
+                p: 4,
+                pt: 2,
+                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+              }}
+            >
+              {image.title}
+              <ImageMarked className="MuiImageMarked-root" />
+            </Typography>
+          </Image>
+        </ImageButton>
+      </Box>
+    </Link>
   );
 }
