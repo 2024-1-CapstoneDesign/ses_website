37d0bfddded0dca32de986e833d7397d78640458
falconlee236 <falconlee236@cau.ac.kr>
2024-04-24 16:24:15 +0900

    add speed-insights

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {QueryClientProvider, QueryClient} from "react-query";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <SpeedInsights />
   </QueryClientProvider>,
   document.getElementById("root")
 );
