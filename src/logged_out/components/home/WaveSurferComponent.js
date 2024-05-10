84d703db8338163ab4a6af1c64dbbab0dbd80604
falconlee236 <falconlee236@cau.ac.kr>
2024-05-10 20:54:22 +0900

    add setDuration in WaveSurferComponent.js with current.getDuration()

--- src/logged_out/components/home/WaveSurferComponent.js
+++ src/logged_out/components/home/WaveSurferComponent.js
@@ -39,7 +39,7 @@ const styles = () => ({
 });
 
 const WaveSurferComponent = (props) => {
-  const {classes, audioURL, setCurrentTime} = props
+  const {classes, audioURL, setCurrentTime, setDuration} = props
   const waveform = useRef(null);
   const wavesurfer = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -61,6 +61,9 @@ const WaveSurferComponent = (props) => {
       wavesurfer.current.on('ready', () => {
         wavesurfer.current.setVolume(0.5);
         wavesurfer.current.pause();
+        if (setDuration){
+          setDuration(wavesurfer.current.getDuration());
+        }
       });
 
       wavesurfer.current.on("finish", () => {
@@ -75,7 +78,7 @@ const WaveSurferComponent = (props) => {
         }
       });
     }
-  },[audioURL, setCurrentTime])
+  },[audioURL, setCurrentTime, setDuration])
 
   const buttonClick = (event) => {
     event.preventDefault();
