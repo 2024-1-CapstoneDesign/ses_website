b805d6a8fc0b8455bd82163fa1a0f3cf82bfa8b0
falconlee236 <falconlee236@cau.ac.kr>
2024-11-24 15:53:24 +0900

    add google search page

--- src/logged_out/components/soundGen/SoundGen.js
+++ src/logged_out/components/soundGen/SoundGen.js
@@ -1,73 +1,98 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import withStyles from "@mui/styles/withStyles";
-import {Box, IconButton, Typography} from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import classNames from "classnames";
+import {Box} from "@mui/material";
 
-const styles = (theme) => ({
-  blogContentWrapper: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(4),
-      marginRight: theme.spacing(4),
-    },
-  },
-  wrapper: {
-    minHeight: "60vh",
-  },
-  titleContainer: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    marginBottom: theme.spacing(5),
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(4),
-      marginRight: theme.spacing(4),
-    },
+const styles = () => ({
+  container: {
+    height: "100vh",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
+    gap: "1rem",
   },
-  searchBorder: {
-    border: "1px solid rgb(229 231 235)",
+  headingContainer: {
+    width: "800px",
+    textAlign: "center",
+  },
+  googleLogo: {
+    width: "25%"
+  },
+  innerContainer: {
+    width: "50%",
     display: "flex",
-    justifyContent: "space-evenly",
-  }
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "0.05rem solid #bfbfbf",
+    borderRadius: "1rem",
+    padding: "1rem",
+    boxShadow: "0px 4px 16px 0px #bfbfbf"
+  },
+  searchBar: {
+    width: "100%",
+    backgroundColor: "transparent",
+    border: "none",
+    fontFamily: 'Roboto',
+    fontSize: "1rem",
+    color: "#64748b",
+    outline: "none",
+    "&:hover": {
+      outline: "none",
+    }
+  },
+  searchIcon: {
+    width: "1rem",
+    marginRight: "1rem",
+  },
+  searchContainer: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "0.5rem",
+  },
 });
 
 function SoundGen(props) {
-  const {classes, theme, selectSoundGen} = props;
+  const {classes,  selectSoundGen} = props;
+  const [input, setInput] = useState("");
+  const inputChange = (e) => {
+    setInput(e.target.value);
+  }
 
   useEffect(() => {
     selectSoundGen();
   }, [selectSoundGen]);
   
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="column"
-      className={classNames(classes.wrapper, "lg-p-top")}
-    >
-      <div className={classes.titleContainer}>
-        <Typography
-          variant="h2"
-          component="h2"
-          fontWeight="400"
-        >
-          SoundEffect Results
-        </Typography>
-        <Box className={classes.searchBorder}>
-          <IconButton type="button" sx={{ p: '10px' }} aria-label="search" disabled>
-            <SearchIcon />
-          </IconButton>
+    <Box className={classes.container}>
+      <Box className={classes.headingContainer}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+          className={classes.googleLogo}
+          alt="google logo"
+        />
+      </Box>
+      <Box className={classes.innerContainer}>
+        <Box className={classes.searchContainer}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+            alt="search icon"
+            className={classes.searchIcon}
+          />
+          <input
+            type="search"
+            className={classes.searchBar}
+            value={input}
+            placeholder="Search Google"
+            onChange={inputChange}
+          />
         </Box>
-      </div>
+      </Box>
     </Box>
   );
-
-
 }
 
-export default withStyles(styles, { withTheme: true })(SoundGen);
+export default withStyles(styles, {withTheme: true})(SoundGen);
